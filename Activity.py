4615a401b19368f47472be1e8c7c9209caa018ae
Michał Salwin <michal.salwin@ergo.com>
2022-04-03 18:12:48 +0200

    Głębszy refaktoring

---
+++ Activity.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+
+class Activity:
+    id:             int = None
+    status:         str = None
+    status_reason:  str = None
+    name:           str = None
+    trainer:        str = None
+    weekday:        str = None
+    date:           datetime = None
+    hour:           str = None
+
+    def print_line(self):
+        print ('{:<7} {:<15} {:<20} {:<30} {:<30} {:<13} {:<13} {:<13}'.
+            format(self.id, 
+                   self.status, 
+                   str(self.status_reason), 
+                   self.name,
+                   self.trainer, 
+                   self.weekday, 
+                   self.date, 
+                   self.hour
+            )
+        )
+    
+    def get_log_found_message(self) -> str:
+        return f"Found Activity {self.name}, status: {self.status} starting at {self.weekday}, {self.date} {self.hour}"
