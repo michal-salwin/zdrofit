e9ffd5695e4d2fed412371453d8762957e59bcf9
Michał Salwin <michal.salwin@ergo.com>
2022-05-11 17:31:52 +0200

    Obsługa nowych klubów

--- zdrofit/Club.py
+++ zdrofit/Club.py
@@ -1,9 +1,12 @@
 class Club:
 
     __clubs = {
-        'gdansk-przymorze': (33,'Gdańsk Przymorze'),
-        'gdansk-manhattan': (32,'Gdańsk Manhattan'),
-        'gdynia-chwarzno':  (43,'Gdynia Chwarzno')
+        'gdansk-przymorze':       (33,'Gdańsk Przymorze'),
+        'gdansk-manhattan':       (32,'Gdańsk Manhattan'),
+        'gdynia-chwarzno':        (43,'Gdynia Chwarzno'),
+        'gdynia-karwiny':         (65,'Gdynia Karwiny'),
+        'gdynia-riviera':         (37,'Gdynia Riviera'),
+        'gdynia-plac-kaszubski':  (76,'Gdynia Plac Kaszubski')
     }
 
     __symbol: str = None
