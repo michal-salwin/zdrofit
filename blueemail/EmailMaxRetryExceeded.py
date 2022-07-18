ee5e0de367cecdd0f6b6bd45ad21708e4ac7ecd8
Michał Salwin <michal.salwin@ergo.com>
2022-07-18 13:29:14 +0200

    poprawki po testach automatycznych

--- blueemail/EmailMaxRetryExceeded.py
+++ blueemail/EmailMaxRetryExceeded.py
@@ -10,9 +10,9 @@ class EmailMaxRetryExceeded(BaseEmail):
                 '<html><body>'
                 '<h3>GymBooker automatyczna rejestracja - problemik :(</h3>'
                 f'<p>{self.user.get_first_name_vocative()},</p>'
-                f'<p>Zapis na zajęcja <b>{self.activity_to_book.name}</b> odbywające się <b>{Weekday.name_pl_acc(self.actactivity_to_bookivity.weekday)}</b> o godz. <b>{self.activity_to_book.hour}</b> w klubie <b>{self.activity_to_book.club.get_name()}</b> nie powiódł się.<br/>'
+                f'<p>Zapis na zajęcia <b>{self.activity_to_book.name}</b> odbywające się <b>{Weekday.name_pl_acc(self.activity_to_book.weekday)}</b> o godz. <b>{self.activity_to_book.hour}</b> w klubie <b>{self.activity_to_book.club.get_name()}</b> nie powiódł się.<br/>'
                 f'Wykonano serię prób rejestracji, żadna nie zakończyła się pomyślnie :-(</p>'
-                '<p>Spróbuj ręcznego zapisu - może jeszcze się uda, zanim zabraknie miejsc ... </p>'
+                '<p>Spróbuj ręcznego zapisu - może jeszcze się uda zanim zabraknie miejsc ... </p>'
                 '</body></html>'
         )
         self.message.content_html = text
